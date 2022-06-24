19cb1a0bc4e4a40e5248899a7ab89c72b92e1dde
Quang Minh Pham <phamquangminhca@Quangs-MacBook-Pro.local>
2022-06-23 22:49:42 -0500

    init

---
+++ src/context/github/GithubContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
+import githubReducer from "./GithubReducer";
+
+
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({children}) => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+
+    // Get initial users (testing purposes)
+    const fetchUsers = async () => {
+
+        const response = await fetch(`${GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+        dispatch({
+            type: 'GET_USERS',
+            payload: data
+        })
+    }
+
+
+    
+    return <GithubContext.Provider value={{
+        ...state,
+        fetchUsers,
+        dispatch
+    }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+
+export default GithubContext
